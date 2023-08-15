bab087bcdba6e47722ebdfba43fed5da19e185b5
Cristian Rossi <cristian.rossicosta@gmail.com>
2023-08-15 03:38:28 -0300

    searchbar round

--- app/components/SearchBar.jsx
+++ app/components/SearchBar.jsx
@@ -5,14 +5,14 @@ export default function SearchBar({ handleSearchNavigate }) {
     <section className="w-full">
       <div className="relative">
         <input
-          className="w-full p-2 text-black font-light text-sm bg-neutral-200 focus:outline-none focus:border-neutral-300 transition duration-300 border-2 border-transparent pr-10"
+          className="w-full p-2 text-black font-light text-sm bg-neutral-200 focus:outline-none focus:border-neutral-300 transition duration-300 border-2 border-transparent pr-10 rounded-sm"
           type="search"
           placeholder="Pesquise por produtos"
           maxLength={30}
           onKeyDown={(e) => handleSearchNavigate(e.key, e.target.value)}
         />
         <div className="absolute inset-y-0 right-0 flex items-center pr-2 pt-1 pointer-events-none">
-          <BiSearchAlt2 className="text-neutral-800 text-lg" />
+          <BiSearchAlt2 className="text-neutral-800 text-lg " />
         </div>
       </div>
     </section>
