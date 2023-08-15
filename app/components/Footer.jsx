bd2ab21606f6164a7f9b6d0a01015e6819ca3045
Cristian Rossi <cristian.rossicosta@gmail.com>
2023-08-15 02:58:28 -0300

    footer padding adjust

--- app/components/Footer.jsx
+++ app/components/Footer.jsx
@@ -2,7 +2,7 @@ import { AiFillGithub } from "react-icons/ai";
 
 export default function Footer() {
   return (
-    <footer className="flex flex-col items-center bg-neutral-50 text-white my-12 px-4">
+    <footer className="flex flex-col items-center bg-neutral-50 text-white my-12 px-8">
       <div className="border-t-2 rounded-lg border-neutral-200 w-full mb-12"></div>
       <a
         className="absolute left-7 pt-10"
